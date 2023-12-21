4e5aadc14913ef2db8a31cf91d53d3f7b2a8e55e
sumanborra <nxtwave@ip-10-2-51-77.ap-south-1.compute.internal>
2023-12-21 07:50:02 +0000

    Doubt not working on browser

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+app.get('/', (request, response) => {
+  response.send('hello')
+})
+app.get('/date', (request, response) => {
+  let date = new Date()
+  response.send(`today's date is ${date}`)
+})
+app.get('/page', (request, response) => {
+  response.sendFile('./page.html', {root: __dirname})
+})
+app.listen(3000)
